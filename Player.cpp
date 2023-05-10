c71613cd051fcdf85a7b71cf690a706d5ebac853
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-05-10 14:05:14 +0900

    a

--- Player.cpp
+++ Player.cpp
@@ -1,14 +1,24 @@
 #include "Player.h"
 #include <cassert>
-void Player::Initialize(Model* model, uint32_t textureHandle) { 
+void Player::Initialize(Model* model, uint32_t textureHandle) {
 	assert(model);
 	model_ = model;
 	textureHandle_ = textureHandle;
 	worldTransform_.Initialize();
+	// シングルトンインスタンスを取得する
+	input_ = Input::GetInstance();
 }
 
-void Player::Update() { worldTransform_.TransferMatrix(); }
-void Player::Draw(ViewProjection view) { 
-	model_->Draw(worldTransform_, view, textureHandle_);
-
+void Player::Update() {
+	worldTransform_.TransferMatrix();
+	Vector3 move = {0, 0, 0};
+	// キャラクターの移動量
+	const float kCharacterSpeed = 0.2f;
+	// 押した方向で移動ベクトルを変更
+	if (input_->PushKey(DIK_LEFT)) {
+		move.x -= kCharacterSpeed;
+	} else if (input_->PushKey(DIK_RIGHT)) {
+		move.x += kCharacterSpeed;
+	}
 }
+void Player::Draw(ViewProjection view) { model_->Draw(worldTransform_, view, textureHandle_); }
