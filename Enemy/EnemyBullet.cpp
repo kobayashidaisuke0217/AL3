126d1fe8b6e21aa1f08bd631f3982c56e16d792f
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-06-07 15:32:35 +0900

    自機の方向にbulletが回転するようになった

--- Enemy/EnemyBullet.cpp
+++ Enemy/EnemyBullet.cpp
@@ -8,7 +8,13 @@ assert(model);
 	model_ = model;
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
+	worldTransform_.scale_ = {0.5f, 0.5f, 3.0f};
 	velocity_ = velocity;
+	// Y軸回り
+	worldTransform_.rotation_.y = std::atan2(velocity_.x, velocity_.z);
+	float velociteXZ = Length({velocity_.x, 0.0f, velocity_.z});
+	//X軸回り
+	worldTransform_.rotation_.x = std::atan2(-velocity_.y, velociteXZ);
 }
 
 void EnemyBullet::Updarte() {
