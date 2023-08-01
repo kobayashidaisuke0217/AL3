9e852dc74f12e05c4748a148924613a6cf76ae21
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-08-01 20:12:10 +0900

    a

--- SkyDome.h
+++ SkyDome.h
@@ -1,7 +1,7 @@
 #pragma once
 #include"3d/Model.h"
 #include "3d/WorldTransform.h"
-#include "3d/ViewProjection.h"
+#include <3d/ViewProjection.h>
 class SkyDome {
 public:
 	void Initialize( Model* model);
