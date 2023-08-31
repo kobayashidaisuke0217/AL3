31cf656bda9fc94acfec7aaf406fcd7fe5142abd
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-08-31 10:25:36 +0900

    Add CollisionManager

---
+++ Manager/Collider/CollisionConfig.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+#include <cstdint>
+namespace CollisionConfig {
+const uint32_t kCollisionAttributePlayer = 0b1;
+const uint32_t kCollisionAttributeEnemy = 0b1 << 1;
+}; // namespace CollisionConfig
